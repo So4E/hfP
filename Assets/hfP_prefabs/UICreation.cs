25317f5f3ac266f5d09343691c3d4765b446d579
So4E <92083201+So4E@users.noreply.github.com>
2024-07-26 15:34:44 +0200

    prototyping panel instantiate

--- Assets/hfP_prefabs/UICreation.cs
+++ Assets/hfP_prefabs/UICreation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UICreation : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class UICreation : MonoBehaviour
     [SerializeField] private GameObject NewObjectWindow;
     [SerializeField] private GameObject UIPrototypingParent;
     [SerializeField] private GameObject UIPanel;
+    [SerializeField] private GameObject UIButton;
+    [SerializeField] private GameObject UIText;
+
     [SerializeField] private GameObject TextEnterWindow;
 
     private GameObject _currentUIPanel;
@@ -17,12 +21,21 @@ public class UICreation : MonoBehaviour
 
     public void SpawnUIPanel()
     {
+        //get user to name his panel
         string _panelName = getInputFromTextEnterWindow();
-        //get coordinates where to spawn panel-> with offset to new object panel
-        Vector3 _positionNextToNewObjectPanel = NewObjectWindow.transform.position + new Vector3(0, 3, 0);
-        GameObject _panel = Instantiate(UIPanel, _positionNextToNewObjectPanel, Quaternion.identity);
+
+        //get coordinates where to spawn panel with offset to new object panel and instantiate it
+        Vector3 _positionNextToNewObjectPanel = NewObjectWindow.transform.position + new Vector3(0.3f, 0, 0);
+        GameObject _panel = Instantiate(UIPanel, _positionNextToNewObjectPanel, Quaternion.identity); //prefab, position, rotation
+        _panel.transform.SetParent(UIPrototypingParent.transform, true); //position stays
+
         //set Header to _panelName
-        _panel.transform.FindChild("Header").GetComponent<TMP_Text>().text = _panelName;
+        GameObject _header = getChildGameObject(_panel, "Header");
+        if(_header == null) { 
+            Debug.Log("header not found");
+            return;
+        }
+        _header.transform.GetComponent<TMP_Text>().SetText(_panelName);
         _currentUIPanel = _panel;
         _createdUIPanels.Add(_panel);
     }
@@ -34,6 +47,14 @@ public class UICreation : MonoBehaviour
         //get input and return it
         string _textInput = "NewPanel";
         return _textInput;
+    }
+
+    private GameObject getChildGameObject(GameObject fromGameObject, string withName)
+    {
+        //Author: Isaac Dart, June-13.
+        Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>();
+        foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
+        return null;
     }
 
 }
