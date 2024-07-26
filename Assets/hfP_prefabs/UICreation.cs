8fcd70826f9b9a9d95eae510321458648562422f
So4E <92083201+So4E@users.noreply.github.com>
2024-07-26 18:12:13 +0200

    further updated ui creation script

--- Assets/hfP_prefabs/UICreation.cs
+++ Assets/hfP_prefabs/UICreation.cs
@@ -13,17 +13,89 @@ public class UICreation : MonoBehaviour
     [SerializeField] private GameObject UIText;
 
     [SerializeField] private GameObject TextEnterWindow;
+    [SerializeField] private TMP_Text TextEnterWindowText;
+    [SerializeField] private GameObject NoPanelSelectedErrorMessage;
+    [SerializeField] private TouchScreenKeyboard keyboard;
 
     private GameObject _currentUIPanel;
-    //List of UIPanels
-    private List<GameObject> _createdUIPanels = new List<GameObject>(); 
+    private List<GameObject> _createdUIPanels = new List<GameObject>();
+    private string _whatToInitialize = "nothing";
 
-
-    public void SpawnUIPanel()
+    private void Update()
     {
-        //get user to name his panel
-        string _panelName = getInputFromTextEnterWindow();
+        if (keyboard != null && TextEnterWindow.activeSelf == true) //&& TextEnterWindow is active
+        {
+            string _keyboardInput = keyboard.text;
+            TextEnterWindowText.GetComponent<TMP_Text>().SetText(_keyboardInput);
+        }
+    }
 
+    public void CreateUIElement(string element)
+    {
+        _whatToInitialize = element;
+        if (element == "panel")
+        {
+            openTextEnterWindow("Please enter a name for your Panel here");
+            return;
+        }
+        if (_currentUIPanel == null)
+        {
+            NoPanelSelectedErrorMessage.SetActive(true);
+            //Todo check if transform needs to be set to offset to newObject window, in case this was placed somewhere else by the user, so that this window is seen by them
+            return;
+        }
+        switch (element)
+        {          
+            case "text":
+                openTextEnterWindow("Please enter your text for the text field here");
+                break;            
+            case "button":
+                openTextEnterWindow("Please enter your label text for the button here");
+                break;            
+            default:
+                //what to do as default?
+                break;
+        }
+    }
+
+    private void openTextEnterWindow(string _description)
+    {
+        TextEnterWindow.SetActive(true);
+        GameObject _descriptionText = getChildGameObject(TextEnterWindow, "Description_Text");
+        _descriptionText.GetComponent<TMP_Text>().SetText(_description);
+    }
+
+    public void OpenSystemKeyboard()
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+    }
+
+    public void OnClickConfirm()
+    {
+        if (keyboard != null)
+        {
+            string _keyboardText = keyboard.text;
+
+            switch (_whatToInitialize)
+            {
+                case "panel":
+                    SpawnUIPanel(_keyboardText);
+                    break;
+                case "text":
+                    SpawnText(_keyboardText);
+                    break;
+                case "button":
+                    SpawnButton(_keyboardText);
+                    break;
+                default:
+                    //todo - what to do as default?
+                    break;
+            }
+        }
+    }
+
+    private void SpawnUIPanel(string _title)
+    {
         //get coordinates where to spawn panel with offset to new object panel and instantiate it
         Vector3 _positionNextToNewObjectPanel = NewObjectWindow.transform.position + new Vector3(0.3f, 0, 0);
         GameObject _panel = Instantiate(UIPanel, _positionNextToNewObjectPanel, Quaternion.identity); //prefab, position, rotation
@@ -31,27 +103,37 @@ public class UICreation : MonoBehaviour
 
         //set Header to _panelName
         GameObject _header = getChildGameObject(_panel, "Header");
-        if(_header == null) { 
+        if (_header == null)
+        {
             Debug.Log("header not found");
             return;
         }
-        _header.transform.GetComponent<TMP_Text>().SetText(_panelName);
+        _header.transform.GetComponent<TMP_Text>().SetText(_title);
         _currentUIPanel = _panel;
         _createdUIPanels.Add(_panel);
     }
 
-    private string getInputFromTextEnterWindow()
+    private void SpawnText(string _inputText)
     {
-        //open text enter window
-        //clear old text // put in placeholder (Button, Text, etc.)
-        //get input and return it
-        string _textInput = "NewPanel";
-        return _textInput;
+        //todo - initialize text and set text to _inputText
+
+        //todo - this text is a differently formatted button to make easy user editing possible on HoloLens -> must be handled differently if ui prototype
+        //is supposed to be used and further edited in UDE
     }
 
+    private void SpawnButton(string _inputText)
+    {
+        //todo - initialize button and set button text to _inputText
+    }
+
+    //*********** further to do for UI prototyping
+    //where to open list of created ui panels, how to open them again and set current panel to that ui
+    //make onClick method for created ui panel to reedit the object created (text, delete, duplicate)
+
+    //utility
     private GameObject getChildGameObject(GameObject fromGameObject, string withName)
     {
-        //Author: Isaac Dart, June-13.
+        //Author: Isaac Dart, June-13. link: https://discussions.unity.com/t/how-to-find-a-child-gameobject-by-name/31255/2
         Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>();
         foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
         return null;
