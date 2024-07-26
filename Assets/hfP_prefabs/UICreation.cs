52a89dd9bd1237fae8bef8a9686c148ef3c9400e
So4E <92083201+So4E@users.noreply.github.com>
2024-07-26 14:37:59 +0200

    started with prototyping ui implementation

--- Assets/hfP_prefabs/UICreation.cs
+++ Assets/hfP_prefabs/UICreation.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class UICreation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject NewObjectWindow;
+    [SerializeField] private GameObject UIPrototypingParent;
+    [SerializeField] private GameObject UIPanel;
+    [SerializeField] private GameObject TextEnterWindow;
+
+    private GameObject _currentUIPanel;
+    //List of UIPanels
+    private List<GameObject> _createdUIPanels = new List<GameObject>(); 
+
+
+    public void SpawnUIPanel()
     {
-        
+        string _panelName = getInputFromTextEnterWindow();
+        //get coordinates where to spawn panel-> with offset to new object panel
+        Vector3 _positionNextToNewObjectPanel = NewObjectWindow.transform.position + new Vector3(0, 3, 0);
+        GameObject _panel = Instantiate(UIPanel, _positionNextToNewObjectPanel, Quaternion.identity);
+        //set Header to _panelName
+        _panel.transform.FindChild("Header").GetComponent<TMP_Text>().text = _panelName;
+        _currentUIPanel = _panel;
+        _createdUIPanels.Add(_panel);
     }
 
-    // Update is called once per frame
-    void Update()
+    private string getInputFromTextEnterWindow()
     {
-        
+        //open text enter window
+        //clear old text // put in placeholder (Button, Text, etc.)
+        //get input and return it
+        string _textInput = "NewPanel";
+        return _textInput;
     }
+
 }
